fbd5d24a3c59d3b7ab21e0f4c849afed69fcbe92
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-03-08 07:42:32 +0800

    fixup: Affine ExynosDisplay to big cores
    Change-Id: Idb12367ee0d29ef4820c815931c482dbfceab8ad

--- libhwc2.1/libdevice/ExynosDisplay.cpp
+++ libhwc2.1/libdevice/ExynosDisplay.cpp
@@ -3679,14 +3679,20 @@ int32_t ExynosDisplay::presentDisplay(int32_t* outRetireFence) {
         setTaskProfileDone = true;
     }
     
-    cpu_set_t cpuset;
-    CPU_ZERO(&cpuset);
-    for (int i = 4; i <= 7; ++i) {
-        CPU_SET(i, &cpuset);
-    }
+    thread_local bool setAffinityDone = false;
 
-    if (sched_setaffinity(0, sizeof(cpu_set_t), &cpuset) != 0) {
-        ALOGW("Failed to set ExynosDisplay CPU affinity to big cores!");
+    if (!setAffinityDone) {
+        cpu_set_t cpuset;
+        CPU_ZERO(&cpuset);
+        for (int i = 4; i <= 7; ++i) {
+            CPU_SET(i, &cpuset);
+        }
+
+        if (sched_setaffinity(gettid(), sizeof(cpu_set_t), &cpuset) != 0) {
+            ALOGW("Failed to set ExynosDisplay CPU affinity to big cores!");
+        } else {
+            setAffinityDone = true;
+        }
     }
 
     Mutex::Autolock lock(mDisplayMutex);
